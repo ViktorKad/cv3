6318c4790260944c74b67fde60d8972da9ab01f9
ViktorKad <kad-dart@yandex.ru>
2017-02-16 19:09:04 +0300

    Верстка для сафари

--- common/contacts/contacts.jn.js
+++ common/contacts/contacts.jn.js
@@ -1,12 +1,16 @@
 jn.create('contacts', function () {
-    var items = ['email', 'links', 'city'].map(function(item) {
-        return jn.exec('contacts__line', {
-            key: '[% lang:contacts.' + item + '.key %]',
-            value: '[% lang:contacts.' + item + '.value %]'
-        });
-    });
+    var items = ['email', 'links', 'city'];
 
     return jn.exec('contacts__layout', {
-        items: items.join('')
+        keys: items.map(function(item) {
+            return jn.exec('contacts__line', {
+                text: '[% lang:contacts.' + item + '.key %]'
+            });
+        }).join(''),
+        values: items.map(function(item) {
+            return jn.exec('contacts__line', {
+                text: '[% lang:contacts.' + item + '.value %]'
+            });
+        }).join('')
     });
 });
